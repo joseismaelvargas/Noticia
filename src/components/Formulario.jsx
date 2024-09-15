648d3750c0219b5a6babfeb7a5cddf150ebd7fd2
ismael <joseismaelvargas@users.noreply.github.com>
2024-09-15 20:29:14 -0300

    Completado

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -10,11 +10,13 @@ function Formulario() {
   // Función para llamar a la API con el valor seleccionado
   const Api = async (valor) => {
     try {
-      const api = await fetch(`https://newsapi.org/v2/everything?q=${valor}&apiKey=aea3419985dd49e29d64da46792d32ea`);
+      
+      const api = await fetch(`https://newsdata.io/api/1/latest?apikey=pub_53274a28b0b7fe9b499f4781c62b8b0e901e7&q=${valor}&language=es`);
+      console.log(api)
       if (api.status === 200) {
         let leer = await api.json();
-        let noti = leer.articles;
-        console.log(noti);
+        let noti = leer.results;
+        console.log(leer);
         setNoticia(noti);
       } else {
         alert("Error en la solicitud: " + api.status);
@@ -47,20 +49,21 @@ function Formulario() {
             <option value="deporte">Deporte</option>
             <option value="tecnologia">Tecnología</option>
             <option value="Criptomoneda">Criptomoneda</option>
+            <option value="politica">Politica</option>
           </Form.Select>
           {errors.opciones && <span>Por favor, selecciona una opción</span>}
           <button className='btn btn-primary mt-3' type='submit'>Buscar</button>
         </Form>
       </div>
 
-      {/* Mostrar noticias */}
+  
       <div className='container cartas mt-4'>
         {noticia.length > 0 ? (
           noticia.map((element, index) => (
             <Cardnoticias 
               key={index} 
               title={element.title} 
-              img={element.urlToImage} 
+              img={element.image_url} 
             />
           ))
         ) : (
