6146d75794ea02c0f6f454dec5704c8b67363241
ismael <joseismaelvargas@users.noreply.github.com>
2024-09-16 17:12:43 -0300

    se agrego el react loading

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,23 +1,28 @@
 import { useEffect, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import { useForm } from "react-hook-form";
+import { Riple } from "react-loading-indicators"
 import Cardnoticias from './Cardnoticias';
 
 function Formulario() {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const [noticia, setNoticia] = useState([]);
+  const [loading,setloading]=useState(true)
 
 
   const Api = async (valor,region) => {
     try {
-      
+    
       const api = await fetch(`https://newsdata.io/api/1/latest?apikey=pub_53274a28b0b7fe9b499f4781c62b8b0e901e7&q=${valor}&language=es&country=${region}`);
       console.log(api)
+       setloading(false)
       if (api.status === 200) {
+       
         let leer = await api.json();
         let noti = leer.results;
         console.log(leer);
         setNoticia(noti);
+
       } else {
         alert("Error en la solicitud: " + api.status);
       }
@@ -28,8 +33,9 @@ function Formulario() {
   };
 
 
-  const buscar = (data) => {
-    if (data.opciones !== 'Opciones'&data.region!=='Opciones') {
+  const buscar = (data) => {  
+    setloading(true)
+    if (data.opciones !== 'Opciones'&&data.region!=='Opciones') {
       Api(data.opciones,data.region); 
     } else {
       alert('Por favor, selecciona una categoría.');
@@ -42,8 +48,9 @@ function Formulario() {
   return (
     <>
       <div className='container'>
-        <h2 className='text-center ml-3'>Buscar por Categoría:</h2>
-        <Form onSubmit={handleSubmit(buscar)}>
+        <h2 className='text-center ml-3 my-3'>Buscar por Categoría:</h2>
+      
+        <Form onSubmit={handleSubmit(buscar)} className='d-flex justify-content-center'>
           <Form.Select className='select' aria-label="Default select example" aria-placeholder='opciones' {...register("opciones", { required: true })}>
             <option value="Opciones">Opciones</option>
             <option value="deporte">Deporte</option>
@@ -61,22 +68,24 @@ function Formulario() {
 
            {errors.region&&<span>Seleciona una region</span>}
           {errors.opciones && <span>Por favor, selecciona una opción</span>}
-          <button className='btn btn-primary mt-3' type='submit'>Buscar</button>
+          <button className='buscar btn btn-primary mt-3' type='submit'>Buscar</button>
         </Form>
       </div>
 
-  
+       <hr />
       <div className='container cartas mt-4'>
-        {noticia.length > 0 ? (
+        {loading===true ? (<Riple color="black" size="50px" text="Espere.." textColor="" /> 
+         
+          )
+         : (
+         
+          
           noticia.map((element, index) => (
             <Cardnoticias 
               key={index} 
               title={element.title} 
               img={element.image_url} 
-            />
-          ))
-        ) : (
-          <p>No hay noticias disponibles para la categoría seleccionada.</p>
+            />))
         )}
       </div>
     </>
